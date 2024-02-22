6e568baafe32e3b37c93488c742e08fef84eccbc
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-02-21 21:52:40 -0600

    34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.ip_calculadora"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ip_calculadora"
